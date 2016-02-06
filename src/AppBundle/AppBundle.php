f25352bc33a944a302b2c1262800ea83187321a4
Sergey <skyeff@gmail.com>
2016-02-06 12:19:13 +0300

    - removed: obsolete code;

--- src/AppBundle/AppBundle.php
+++ src/AppBundle/AppBundle.php
@@ -2,7 +2,6 @@
 
 namespace AppBundle;
 
-use AppBundle\DependencyInjection\Compiler\FormBuilderCompilerPass;
 use AppBundle\DependencyInjection\Compiler\GoalBuilderCompilerPass;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
@@ -13,7 +12,6 @@ class AppBundle extends Bundle
     {
         parent::build($container);
 
-        $container->addCompilerPass(new FormBuilderCompilerPass());
         $container->addCompilerPass(new GoalBuilderCompilerPass());
     }
 
--- src/AppBundle/Templates/BuilderInterface.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace AppBundle\Templates;
-
-use Symfony\Component\Form\FormBuilderInterface;
-
-interface BuilderInterface {
-    public function build($alias, FormBuilderInterface $builder);
-}
